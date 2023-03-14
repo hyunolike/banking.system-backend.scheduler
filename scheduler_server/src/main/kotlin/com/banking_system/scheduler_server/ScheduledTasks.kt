e717c7f57e93c9a25fef6bd609d714cc1d8ae633
hh.jang <fallear1@gmail.com>
2023-03-14 20:10:29 +0900

    feat: get 요청 추가 (#3)

--- scheduler_server/src/main/kotlin/com/banking_system/scheduler_server/ScheduledTasks.kt
+++ scheduler_server/src/main/kotlin/com/banking_system/scheduler_server/ScheduledTasks.kt
@@ -1,14 +1,31 @@
 package com.banking_system.scheduler_server
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.ResponseEntity
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
 
 @Component
 @EnableAsync
 class ScheduledTasks {
 
     // 작업 예약
-    @Scheduled(cron = "0/1 * * * * *")
-    fun taskScheduler() = println("1초마다 실행")
+    @Scheduled(cron = "0/10 * * * * *")
+    fun taskScheduler() {
+         var uri: URI = UriComponentsBuilder
+             .fromUriString("http://localhost:8182/api/userDatas")
+             .encode()
+             .build()
+             .toUri()
+
+        var restTemplate: RestTemplate = RestTemplate()
+
+        return restTemplate.getForObject(uri)
+    }
 }
